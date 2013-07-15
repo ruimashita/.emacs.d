0b3e5e060db58bd53a44a972d408691e83ebd13d
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2013-07-16 00:19:11 +0900

    Disable multi web mode

--- init.el
+++ init.el
@@ -885,13 +885,13 @@
 ;;=========================
 ;; multi-web-mode
 ;;=================================
-(require 'multi-web-mode)
-(setq mweb-default-major-mode 'sgml-mode)
-(setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-                  (js-mode "<script[^>]*>" "</script>")
-                  (css-mode "<style[^>]*>" "</style>")))
-(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
-(multi-web-global-mode 1)
+;; (require 'multi-web-mode)
+;; (setq mweb-default-major-mode 'sgml-mode)
+;; (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+;;                   (js-mode "<script[^>]*>" "</script>")
+;;                   (css-mode "<style[^>]*>" "</style>")))
+;; (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+;;(multi-web-global-mode 1)
 
 
 
