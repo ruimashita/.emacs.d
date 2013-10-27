1fb450d12d508e75f67ec4a6c23eb6eb2304895e
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2013-10-28 05:58:17 +0900

    Add markdown mode

--- init.el
+++ init.el
@@ -425,6 +425,17 @@
 
 
 ;;=======================================================================
+;; markdown-mode
+;;=====================================================================
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+;; (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+
+;;=======================================================================
 ;; yasnippet
 ;;=====================================================================
 (require 'yasnippet)
