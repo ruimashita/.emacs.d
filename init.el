67acf6e0d1bb52fc2f257f28c1939df9bb24f945
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2013-10-29 14:26:32 +0900

    Edit markdown

--- init.el
+++ init.el
@@ -433,6 +433,8 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 ;; (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+(setq markdown-css-path "/home/takuya/local/bootstrap/dist/css/bootstrap.min.css")
+;;(setq markdown-xhtml-header-content " <link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"/home/takuya/local/bootstrap/dist/css/bootstrap.min.css\" /> ")
 
 ;;=======================================================================
 ;; yasnippet
