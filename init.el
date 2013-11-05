b3b3cf3575d61f48dc8373b25378ee457a8b939c
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2013-11-05 14:26:37 +0900

    python マジックコメント

--- init.el
+++ init.el
@@ -634,25 +634,25 @@
           ((string-match "utf-8" coding-str) 'utf-8)
           (t (or default-coding 'utf-8)))))
 
-(defun my-insert-file-local-coding ()
-  "ファイルの先頭に `coding:' を自動挿入する"
-  (interactive)
-  (save-excursion
-    (goto-line 2) (end-of-line) ; ２行目の行末の移動
-    (let ((limit (point)))
-      (goto-char (point-min))
-      (unless (search-forward "coding:" limit t) ; 2行目以内に `coding:'がない
-        (goto-char (point-min))
-        ;; #!で始まる場合２行目に記述
-        (when (and (< (+ 2 (point-min)) (point-max))
-                   (string= (buffer-substring (point-min) (+ 2 (point-min))) "#!"))
-          (unless (search-forward "\n" nil t) ; `#!'で始まり末尾に改行が無い場合
-            (insert "\n"))) ; 改行を挿入
-        (let ((st (point)))
-          (insert (format "-*-coding=%S-*-\n" (my-short-buffer-file-coding-system)))
-          (comment-region st (point)))))))
+;; (defun my-insert-file-local-coding ()
+;;   "ファイルの先頭に `coding:' を自動挿入する"
+;;   (interactive)
+;;   (save-excursion
+;;     (goto-line 2) (end-of-line) ; ２行目の行末の移動
+;;     (let ((limit (point)))
+;;       (goto-char (point-min))
+;;       (unless (search-forward "coding=" limit t) ; 2行目以内に `coding:'がない
+;;         (goto-char (point-min))
+;;         ;; #!で始まる場合２行目に記述
+;;         (when (and (< (+ 2 (point-min)) (point-max))
+;;                    (string= (buffer-substring (point-min) (+ 2 (point-min))) "#!"))
+;;           (unless (search-forward "\n" nil t) ; `#!'で始まり末尾に改行が無い場合
+;;             (insert "\n"))) ; 改行を挿入
+;;         (let ((st (point)))
+;;           (insert (format "-*-coding=%S-*-\n" (my-short-buffer-file-coding-system)))
+;;           (comment-region st (point)))))))
 
-(add-hook 'python-mode-hook 'my-insert-file-local-coding)
+;; (add-hook 'python-mode-hook 'my-insert-file-local-coding)
 
 
 
