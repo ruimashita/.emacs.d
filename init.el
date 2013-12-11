63736dcd12111e40494ebbb9066a258711166820
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2013-12-12 03:51:52 +0900

    edit tramp

--- init.el
+++ init.el
@@ -290,9 +290,9 @@
 (require 'tramp)
 ;;(setq tramp-default-method "ssh")
 (add-to-list 'tramp-default-proxies-alist
-             '(".*" "\\`root\\'" "/ssh:%h:"))
+             '(nil "\\`root\\'" "/ssh:%h:"))
 (add-to-list 'tramp-default-proxies-alist
-             '("localhost" "\\`root\\'" nil))
+             '("localhost" nil nil))
 (add-to-list 'tramp-default-proxies-alist
              '((regexp-quote (system-name)) "\\`root\\'" nil))
 
