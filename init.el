2e3116efdfac2d5ef5dd6439d9dfe9fcda37f808
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2014-02-02 18:06:18 +0900

    utf8の設定

--- init.el
+++ init.el
@@ -119,14 +119,14 @@
 
 ;; 言語・文字コード関連の設定
 (set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-buffer-file-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
 (setq file-name-coding-system 'utf-8)
-
+(setq-default save-buffer-coding-system 'utf-8)
 
 
 
