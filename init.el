a2e32d81550048ab3d99052e11d03e4b42bc3f1f
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2014-11-18 13:59:49 +0900

    web-mode ac

--- init.el
+++ init.el
@@ -844,8 +844,8 @@
      )
   )
 (add-hook 'web-mode-hook
-		  '(lambda ()
-			 (setq indent-tabs-mode t)
+          '(lambda ()
+             (setq indent-tabs-mode t)
 			 (setq web-mode-markup-indent-offset 4)
 			 (setq web-mode-css-indent-offset 4)
 			 (setq web-mode-code-indent-offset 4)
@@ -864,6 +864,7 @@
         )
 )
 
+(add-to-list 'ac-modes 'web-mode)
 ;;=========================
 ;; jinja2-mode
 ;;=================================
