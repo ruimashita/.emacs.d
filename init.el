2269ffd2d2bca9719c12b428ccfbbc9b338cb80a
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2014-12-04 15:32:04 +0900

    yasnippet

--- init.el
+++ init.el
@@ -459,8 +459,10 @@
 ;;=====================================================================
 ;;(require 'auto-complete)
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/dict")
 (ac-config-default)
+
+;; 補完メニュー表示時のキーマップを有効にする
+(setq ac-use-menu-map t)
 
 ;; (global-auto-complete-mode t)
 (ac-set-trigger-key "TAB")
@@ -507,8 +509,8 @@
 ;;=======================================================================
 ;; yasnippet
 ;;=====================================================================
-(require 'yasnippet)
-(yas-global-mode 1)
+;;(require 'yasnippet)
+;;(yas-global-mode 1)
 
 
 ;;=======================================================================
