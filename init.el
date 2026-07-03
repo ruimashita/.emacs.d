8b7aba8b96fb62256ef383200d340c03a2629225
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2026-07-03 21:11:49 +0900

    Update markdown preview css

--- init.el
+++ init.el
@@ -548,8 +548,15 @@
   (setq markdown-command "comrak")
   :custom
   ;; markdown-preview (C-c C-C p)
-  (markdown-xhtml-header-content "<meta charset=\"utf-8\" />
-  <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB\" crossorigin=\"anonymous\">")
+  (markdown-xhtml-header-content "
+<meta charset=\"utf-8\" />
+<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+<link
+  rel=\"stylesheet\"
+  href=\"https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css\" >"
+                                 )
+  (markdown-xhtml-body-preamble "<main class=\"container\">")
+  (markdown-xhtml-body-epilogue "</main>")
   )
 
 
